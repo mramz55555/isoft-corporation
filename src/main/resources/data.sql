5e8cc7e79e69547749f5c107b69f6bc297f869bf
mramz55555 <mramz55555@gmail.com>
2023-02-01 21:02:46 +0330

    commiting all changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,61 @@
-insert into offer (`value`, `date`, `reason`, `type`, `created_at`, `created_by`)
-values (50, '2022-2-1', 'eid al-fitr', 'FESTIVAL', now(), 'DBA');
-insert into offer (`value`, `date`, `reason`, `type`, `created_at`, `created_by`)
-values (39, '2022-4-12', 'Arash kamangir''s eid', 'AZADI', now(), 'DBA');
-insert into offer (`value`, `date`, `reason`, `type`, `created_at`, `created_by`)
-values (66, '2022-6-24', 'eid al-qadir', 'FESTIVAL', now(), 'DBA');
-insert into offer (`value`, `date`, `reason`, `type`, `created_at`, `created_by`)
-values (49, '2022-11-22', '22 bahman''s holiday', 'AZADI', now(), 'DBA');
-insert into offer (`value`, `date`, `reason`, `type`, `created_at`, `created_by`)
-values (35, '2023-1-1', 'eid noroz', 'FESTIVAL', now(), 'DBA');
+insert into offer (id, `value`, `date`, `reason`, `type`, `created_at`, `created_by`)
+values (1, 50, '2022-2-1', 'eid al-fitr', 'FESTIVAL', now(), 'DBA')
+     , (2, 39, '2022-4-12', 'Arash kamangir''s eid', 'AZADI', now(), 'DBA')
+     , (3, 66, '2022-6-24', 'eid al-qadir', 'FESTIVAL', now(), 'DBA')
+     , (4, 49, '2022-11-22', '22 bahman''s holiday', 'AZADI', now(), 'DBA')
+     , (5, 35, '2023-1-1', 'eid noroz', 'FESTIVAL', now(), 'DBA');
 
-insert into role (name, created_at, created_by)
-values ('ROLE_ADMIN', curdate(), 'DBA');
-insert into role(name, created_at, created_by)
-values ('ROLE_CUSTOMER', curdate(), 'DBA');
-insert into User (name, email, mobile_number, password, role_id, created_at, created_by)
-values ('admin', 'mramz55555@gmail.com', '09199658784', '$2a$10$Ct7Pwoo5zirXT8uYqiaIDe2fmQAHyOvteBDQWMa2NWxcEdCQ9Ek1e',
+insert into role (id, name, created_at, created_by)
+values (1, 'ROLE_ADMIN', curdate(), 'DBA')
+     , (2, 'ROLE_CUSTOMER', curdate(), 'DBA');
+insert into user (id, name, email, mobile_num, password, role_id, created_at, created_by)
+values (3, 'admin', 'mramz55555@gmail.com', '09199658784',
+        '$2a$10$Ct7Pwoo5zirXT8uYqiaIDe2fmQAHyOvteBDQWMa2NWxcEdCQ9Ek1e',
         1, curdate(), 'DBA');
+
+
+insert into student (id, name)
+values (1, 'ali');
+insert into student (id, name)
+values (2, 'mohammad');
+insert into student (id, name)
+values (3, 'zamani');
+insert into student (id, name)
+values (4, 'hassan');
+insert into student (id, name)
+values (5, 'hossein');
+insert into student (id, name)
+values (6, 'ahmad');
+
+insert into course (id, name, student_id)
+values (1, 'a', 1);
+
+insert into course (id, name, student_id)
+values (2, 'b', 1);
+
+insert into course (id, name, student_id)
+values (3, 'c', 2);
+
+insert into course (id, name, student_id)
+values (4, 'd', 3);
+
+insert into course (id, name, student_id)
+values (5, 'e', 4);
+
+insert into course (id, name, student_id)
+values (6, 'f', 4);
+
+insert into course (id, name, student_id)
+values (7, 'a', 5);
+
+insert into course (id, name, student_id)
+values (8, 'a', 5);
+
+insert into course (id, name, student_id)
+values (9, 'a', 6);
+
+insert into course (id, name, student_id)
+values (10, 'a', 6);
+
+insert into course (id, name, student_id)
+values (11, 'a', 6);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,71 +1,172 @@
 use
-`isoft-db`;
-CREATE TABLE contact_message
+`isoft_db`;
+
+CREATE TABLE if not exists contact_message
 (
-    `id`         int auto_increment PRIMARY KEY,
-    `name`       varchar(100) NOT NULL,
-    `mobile_num` varchar(11)  NOT NULL,
-    `email`      varchar(100) NOT NULL,
-    `subject`    varchar(100) NOT NULL,
-    `message`    varchar(500) NOT NULL,
-    `status`     varchar(10)  NOT NULL,
-    `created_at` TIMESTAMP    NOT NULL,
-    `created_by` varchar(50)  NOT NULL,
+    `id`
+    int
+    auto_increment
+    PRIMARY
+    KEY,
+    `name`
+    varchar
+(
+    100
+) NOT NULL,
+    `mobile_num` varchar
+(
+    11
+) NOT NULL,
+    `email` varchar
+(
+    100
+) NOT NULL,
+    `subject` varchar
+(
+    100
+) NOT NULL,
+    `message` varchar
+(
+    500
+) NOT NULL,
+    `status` varchar
+(
+    10
+) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    `created_by` varchar
+(
+    50
+) NOT NULL,
     `updated_at` TIMESTAMP   DEFAULT NULL,
     `updated_by` varchar(50) DEFAULT NULL
 );
 
 
-CREATE TABLE offer
+CREATE TABLE if not exists offer
 (
-    `id`         int auto_increment,
-    `value`      int,
-    `date`       varchar(20)  NOT NULL,
-    `reason`     varchar(100) NOT NULL,
-    `type`       varchar(20)  NOT NULL,
-    `created_at` TIMESTAMP    NOT NULL,
-    `created_by` varchar(50)  NOT NULL,
-    `updated_at` TIMESTAMP   DEFAULT NULL,
-    `updated_by` varchar(50) DEFAULT NULL,
+    `id`
+    int
+    auto_increment,
+    `value`
+    int,
+    `date`
+    varchar
+(
+    20
+) NOT NULL,
+    `reason` varchar
+(
+    100
+) NOT NULL,
+    `type` varchar
+(
+    20
+) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    `created_by` varchar
+(
+    50
+) NOT NULL,
+    `updated_at` TIMESTAMP DEFAULT NULL,
+    `updated_by` varchar
+(
+    50
+) DEFAULT NULL,
     primary key (id)
 );
 
-create table role
+create table if not exists role
 (
-    id         int         not null auto_increment,
-    name       varchar(50) not null,
-    created_at timestamp   not null,
-    created_by varchar(50) not null,
-    updated_at timestamp   default null,
-    update_by  varchar(50) default null,
+    `id`
+    int
+    not
+    null
+    auto_increment
+    primary
+    key,
+    name
+    varchar
+(
+    50
+) not null,
+    created_at timestamp not null,
+    created_by varchar
+(
+    50
+) not null,
+    updated_at timestamp default null,
+    update_by varchar
+(
+    50
+) default null
+    );
+
+create table if not exists address
+(
+    id
+    int
+    auto_increment
+    not
+    null,
+    address_one
+    varchar
+(
+    200
+) not null,
+    address_two varchar
+(
+    200
+) not null,
+    city varchar
+(
+    50
+) not null,
+    zip_code int not null,
+    created_at timestamp not null,
+    created_by varchar
+(
+    50
+) not null,
+    updated_at timestamp default null,
+    update_by varchar
+(
+    50
+) default null,
     primary key (id)
 );
 
-create table address
+create table if not exists user
 (
-    id          int auto_increment not null,
-    address_one varchar(200) not null,
-    address_two varchar(200) not null,
-    city        varchar(50)  not null,
-    zip_code    int          not null,
-    created_at  timestamp    not null,
-    created_by  varchar(50)  not null,
-    updated_at  timestamp   default null,
-    update_by   varchar(50) default null,
-    primary key (id)
-);
-
-create table user
+    id
+    int
+    auto_increment
+    not
+    null,
+    name
+    varchar
 (
-    id            int auto_increment not null,
-    name          varchar(50) not null,
-    email         varchar(50) not null,
-    mobile_number varchar(11) not null,
-    password      varchar(50) not null,
-    role_id       int         not null,
-    address_id    int null,
-    created_at    timestamp   not null,
-    created_by    varchar(50) not null,
+    50
+) not null,
+    email varchar
+(
+    50
+) not null,
+    mobile_number varchar
+(
+    11
+) not null,
+    password varchar
+(
+    50
+) not null,
+    role_id int not null,
+    address_id int null,
+    created_at timestamp not null,
+    created_by varchar
+(
+    50
+) not null,
     updated_at    timestamp   default null,
     update_by     varchar(50) default null,
     primary key (id),
@@ -74,111 +175,147 @@ create table user
 );
 
 
-create table class
+create table if not exists class
 (
-    id         int auto_increment not null,
-    name       varchar(100) not null,
-    created_at timestamp    not null,
-    created_by varchar(50)  not null,
-    updated_at timestamp    not null,
-    updated_by varchar(50)  not null,
-    primary key (id)
-);
+    id
+    int
+    auto_increment
+    not
+    null,
+    name
+    varchar
+(
+    100
+) not null,
+    created_at timestamp not null,
+    created_by varchar
+(
+    50
+) not null,
+    updated_at timestamp not null,
+    updated_by varchar
+(
+    50
+) not null,
+    primary key
+(
+    id
+)
+    );
 
 alter table user
     add column class_id int null after address_id,
-    add constraint CLASS_ID_FK foreign key(app_id) references class(id);
+    add constraint CLASS_ID_FK foreign key(class_id) references class(id);
 
 
-create table app
+create table if not exists app
 (
-    id         int          not null auto_increment,
-    name       varchar(100) not null,
-    cost       varchar(10)  not null,
-    created_at timestamp    not null,
-    created_by varchar(50)  not null,
-    updated_at timestamp    not null,
-    updated_by varchar(50)  not null,
-    primary key (id)
-);
-
-create table app_user
+    id
+    int
+    not
+    null
+    auto_increment,
+    name
+    varchar
 (
-    app_id  int not null,
-    user_id int not null,
-    foreign key (app_id) references app (id),
-    foreign key (user_id) references user (id),
-    primary key (app_id, user_id)
-);
-
-
-
-use `isoft-db`;
-
-CREATE TABLE student
+    100
+) not null,
+    cost varchar
 (
-    id   int PRIMARY KEY,
-    name varchar(30) default ''
-);
-
-insert into student (id, name)
-values (1, 'ali');
-insert into student (id, name)
-values (2, 'mohammad');
-insert into student (id, name)
-values (3, 'zamani');
-insert into student (id, name)
-values (4, 'hassan');
-insert into student (id, name)
-values (5, 'hossein');
-insert into student (id, name)
-values (6, 'ahmad');
-
-
-CREATE TABLE course
+    10
+) not null,
+    created_at timestamp not null,
+    created_by varchar
 (
-    id         int PRIMARY KEY,
-    name       varchar(30) default '',
+    50
+) not null,
+    updated_at timestamp not null,
+    updated_by varchar
+(
+    50
+) not null,
+    primary key
+(
+    id
+)
+    );
+
+create table if not exists app_user
+(
+    app_id
+    int
+    not
+    null,
+    user_id
+    int
+    not
+    null,
+    foreign
+    key
+(
+    app_id
+) references app
+(
+    id
+),
+    foreign key
+(
+    user_id
+) references user
+(
+    id
+),
+    primary key
+(
+    app_id,
+    user_id
+)
+    );
+
+
+
+use
+`isoft_db`;
+
+CREATE table if not exists student
+(
+    id
+    int
+    PRIMARY
+    KEY,
+    name
+    varchar
+(
+    30
+) default ''
+    );
+
+
+CREATE table if not exists course
+(
+    id
+    int
+    PRIMARY
+    KEY,
+    name
+    varchar
+(
+    30
+) default '',
     student_id int,
-    foreign KEY (student_id) references student (id)
-);
+    foreign KEY
+(
+    student_id
+) references student
+(
+    id
+)
+    );
 
 alter table student
     add column course_id int null after name,
     add constraint COURSE_ID_FK foreign key (course_id) references course (id);
 
-insert into course (id, name, student_id)
-values (1, 'a', 1);
-
-insert into course (id, name, student_id)
-values (2, 'b', 1);
-
-insert into course (id, name, student_id)
-values (3, 'c', 2);
-
-insert into course (id, name, student_id)
-values (4, 'd', 3);
-
-insert into course (id, name, student_id)
-values (5, 'e', 4);
-
-insert into course (id, name, student_id)
-values (6, 'f', 4);
-
-insert into course (id, name, student_id)
-values (7, 'a', 5);
-
-insert into course (id, name, student_id)
-values (8, 'a', 5);
-
-insert into course (id, name, student_id)
-values (9, 'a', 6);
-
-insert into course (id, name, student_id)
-values (10, 'a', 6);
-
-insert into course (id, name, student_id)
-values (11, 'a', 6);
 
 
 # select *
